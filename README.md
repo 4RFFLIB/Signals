9aaa0e7df09dcea1e222a91d5a9cf3b2f6cb93c4
syed93uthman <syed_uthman@yahoo.com>
2019-12-04 14:44:18 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Signal Library
+This repo is manage signal between lib to lib to main sketch. The main purpose of this library is to isolate between custom library in order to avoid depends between the library. In hope to make the custom library easily changable.
---
+++ src/Signals.h
@@ -0,0 +1,112 @@
+#ifndef _SIGNALS_h
+#define _SIGNALS_h
+
+
+template<typename...> class Signals;
+
+//void function parameters
+template <>
+class Signals<>
+{
+private:
+	bool flag;
+
+public:
+	Signals() {
+		flag = false;
+	}
+
+	template <typename SLOT>
+	void connect(SLOT funcSlot, bool keepsignal) {
+		if (flag) {
+			funcSlot();
+			flag = keepsignal;
+		}
+	}
+
+	template <typename OBJECT, typename SLOT>
+	void connect(OBJECT *obj, SLOT funcSlot, bool keepsignal) {
+		if (flag) {
+			(obj->*funcSlot)();
+			flag = keepsignal;
+		}
+	}
+
+	void emit(void) {
+		flag = true;
+	}
+};
+
+//1 function parameters
+template <typename A>
+class Signals<A>
+{
+private:
+	A _param1;
+	bool flag;
+
+public:
+	Signals() {
+		flag = false;
+	}
+
+	template <typename SLOT>
+	void connect(SLOT funcSlot, bool keepsignal) {
+		if (flag) {
+			funcSlot(_param1);
+			flag = keepsignal;
+		}
+	}
+
+	template <typename OBJECT, typename SLOT>
+	void connect(OBJECT *obj, SLOT funcSlot, bool keepsignal) {
+		if (flag) {
+			(obj->*funcSlot)(_param1);
+			flag = keepsignal;
+		}
+	}
+
+	void emit(A param1) {
+		_param1 = param1;
+		flag = true;
+	}
+};
+
+//2 function parameters
+template <typename A, typename B>
+class Signals<A, B>
+{
+private:
+	A _param1;
+	B _param2;
+	bool flag;
+
+public:
+	Signals() {
+		flag = false;
+	}
+
+	template <typename SLOT>
+	void connect(SLOT funcSlot, bool keepsignal) {
+		if (flag) {
+			funcSlot(_param1, _param2);
+			flag = keepsignal;
+		}
+	}
+
+	template <typename OBJECT, typename SLOT>
+	void connect(OBJECT *obj, SLOT funcSlot, bool keepsignal) {
+		if (flag) {
+			(obj->*funcSlot)(_param1, _param2);
+			flag = keepsignal;
+		}
+	}
+
+	void emit(A param1, B param2) {
+		_param1 = param1;
+		_param2 = param2;
+		flag = true;
+	}
+};
+
+#endif
